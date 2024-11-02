cf64844cab4c67993f21426b10f33f3e23c25fa0
Costel <costelmartinescu2000@gmail.com>
2024-11-02 12:42:57 +0200

    test

--- routes/delete.js
+++ routes/delete.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 require('dotenv').config();
+const path = require('path');
 
 const Nunti = require("../schema/photo1Schema");
 const Portrete = require("../schema/photo2Schema");
@@ -101,15 +102,24 @@ router.delete('/onePhoto', checkAuthenticated, async (req, res) => {
 
 });
 
+async function archiveFolder(folderPath) {
+  const archivePath = path.join(path.dirname(folderPath), `arhiva_${path.basename(folderPath)}`);
+  try {
+    fs.renameSync(folderPath, archivePath);
+    console.log(`Folderul a fost redenumit: ${archivePath}`);
+  } catch (error) {
+    console.error('Eroare la redenumirea folderului:', error);
+    throw error;
+  }
+}
 
-router.delete('/:category/:id', checkAuthenticated, (req, res) => {
-
+// Ruta pentru a șterge un eveniment întreg pe baza categoriei și id-ului
+router.delete('/:category/:id', checkAuthenticated, async (req, res) => {
   const param = req.params.category;
   const id = req.params.id;
-
   let collection;
 
-
+  // Selectăm colecția corespunzătoare categoriei
   switch (param) {
     case 'nunti':
       collection = Nunti;
@@ -131,20 +141,25 @@ router.delete('/:category/:id', checkAuthenticated, (req, res) => {
       return;
   }
 
+  // Ștergem documentul specificat din colecție
   const query = { _id: id };
+  try {
+    const result = await collection.deleteOne(query);
+    if (result.deletedCount > 0) {
+      res.send({ message: 'Evenimentul a fost sters din DB!' });
 
-  collection.deleteOne(query)
-    .then((result) => {
-      if (result.deletedCount > 0) {
-        res.send({ message: 'Evenimentul a fost sters din DB!' });
-      } else {
-        res.status(404).send({ error: 'Evenimentul nu a fost gasit in DB!' });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500);
-    });
+      // Redenumim folderul asociat colecției
+      const folderPath = `./public/uploads/${param}`;
+      await archiveFolder(folderPath);
+
+      // Redenumim colecția în baza de date
+      await archiveCollection(collection);
+    } else {
+      res.status(404).send({ error: 'Evenimentul nu a fost gasit in DB!' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'Eroare la ștergere!' });
+  }
 });
-
 module.exports = router;
