a8e2861b96561aabc47e121e8370a0891d8fc547
Costel <costelmartinescu2000@gmail.com>
2024-11-02 22:03:25 +0200

    pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,60 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', 
+      manifest: {
+        name: 'Ursu Ioan Fotograf"',
+        short_name: 'Portofoliu',
+        description: 'Cele mai interesante colecții sunt aici!',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/fav/icon1.png' ,
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/fav/icon2.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => 
+              request.destination === 'document' ||
+              request.destination === 'script' ||
+              request.destination === 'style' ||
+              request.destination === 'image',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'my-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 30 * 24 * 60 * 60, 
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor'; 
+            return 'vendor';
           }
           if (id.includes('pages')) {
-            return 'pages'; 
+            return 'pages';
           }
         },
       },
